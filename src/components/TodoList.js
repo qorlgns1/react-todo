11f4e1549e3fc7fd4618b794bbfa82ceabed3bff
qorlgns1 <rlgns0610@gmail.com>
2021-09-02 01:00:03 +0900

    수정(토글)기능 구현

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,11 +2,16 @@ import React from 'react';
 import TodoListitem from './TodoListItem';
 import './TodoList.scss';
 
-const TodoList = ({ todos, onRemove }) => {
+const TodoList = ({ todos, onRemove, onToggle }) => {
   return (
     <div className="TodoList">
       {todos.map((todo) => (
-        <TodoListitem todo={todo} key={todo.id} onRemove={onRemove} />
+        <TodoListitem
+          todo={todo}
+          key={todo.id}
+          onRemove={onRemove}
+          onToggle={onToggle}
+        />
       ))}
     </div>
   );
