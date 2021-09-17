92773cb2a9045cc5a6d6396947a3d8481ee861e1
qorlgns1 <rlgns0610@gmail.com>
2021-09-17 15:43:00 +0900

    TodoList 컴포넌트 최적화하기

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ const App = () => {
   const onInsert = useCallback((text) => {
     const todo = {
       id: nextId.current,
-      text,
+      text: `할 일 ${nextId.current}`,
       checked: false,
     };
     setTodos((todos) => todos.concat(todo));
@@ -36,16 +36,13 @@ const App = () => {
     setTodos((todos) => todos.filter((todo) => todo.id !== id));
   }, []);
 
-  const onToggle = useCallback(
-    (id) => {
-      setTodos((todos) =>
-        todos.map((todo) =>
-          todo.id === id ? { ...todo, checked: !todo.checked } : todo,
-        ),
-      );
-    },
-    [todos],
-  );
+  const onToggle = useCallback((id) => {
+    setTodos((todos) =>
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, checked: !todo.checked } : todo,
+      ),
+    );
+  }, []);
 
   return (
     <TodoTemplate>
